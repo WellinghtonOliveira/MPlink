749d3a231e9732e258eeb32f4d3c71dc97674dfd
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-07 10:59:41 -0300

    Update renderer.js

--- Electron/src/renderer.js
+++ Electron/src/renderer.js
@@ -1,20 +1,27 @@
 const urlInput = document.getElementById('input-link')
 const downloadBtn = document.getElementById('button-convert')
 const log = document.getElementById('log')
+const listaBainxando = document.getElementById("lista-baixando")
+const listaBaixados = document.getElementById("lista-baixados")
+let i = 0   
 
 downloadBtn.addEventListener('click', async () => {
   let url = urlInput.value.trim()
 
   urlInput.value = ""
-
+  
   if (!url) {
-    log.textContent += '\n[Erro] Link vazio'
+    addBaixando(i)
+    i++
+    //log.textContent += '\n[Erro] Link vazio'
     return
   }
 
+  exBaixando(toString(urlInput))
+
   url = url.split('?')[0]
 
-  log.textContent += `\n[Iniciando download] ${url}`
+  //log.textContent += `\n[Iniciando download] ${url}`
   try {
     const titulo = await window.api.extrairTitulo(url)
     log.textContent = titulo
@@ -26,15 +33,22 @@ downloadBtn.addEventListener('click', async () => {
   }
 })
 
-
-function addBaixando() {
-
+function addBaixando(titulo) {
+  const novoElementoMusica = document.createElement("li")
+  novoElementoMusica.textContent = titulo
+  listaBainxando.appendChild(novoElementoMusica)
 }
 
 function addBaixados() {
 
 }
 
-function exBaixando() {
-  
+function exBaixando(titulo) {
+  let nomeFilhoLi = listaBainxando.querySelectorAll("li")
+
+  nomeFilhoLi.forEach((el) => {
+    if (el.textContent == titulo) {
+      listaBainxando.removeChild(el)
+    }
+  })
 }
