af644a53220730a9e60d3271d6984b630c78aea8
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-07 10:12:42 -0300

    Update preload.js

--- Electron/preload.js
+++ Electron/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('ytDownloader', {
+contextBridge.exposeInMainWorld('api', {
+  extrairTitulo: (url) => ipcRenderer.invoke('extrair-titulo', url),
   baixarAudio: (url, index) => ipcRenderer.invoke('baixar-audio', url, index)
 })
