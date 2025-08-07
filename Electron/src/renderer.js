263ed1b5cf55a3f269e7bd9015b7d1a434c7381c
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-08-07 10:12:45 -0300

    Update renderer.js

--- Electron/src/renderer.js
+++ Electron/src/renderer.js
@@ -4,19 +4,37 @@ const log = document.getElementById('log')
 
 downloadBtn.addEventListener('click', async () => {
   let url = urlInput.value.trim()
+
+  urlInput.value = ""
+
   if (!url) {
     log.textContent += '\n[Erro] Link vazio'
     return
   }
 
-  // 🔧 Limpar parâmetros indesejados
   url = url.split('?')[0]
 
   log.textContent += `\n[Iniciando download] ${url}`
   try {
-    const resposta = await window.ytDownloader.baixarAudio(url, 1)
-    log.textContent += `\n[Sucesso] ${resposta}`
+    const titulo = await window.api.extrairTitulo(url)
+    log.textContent = titulo
+
+    const resposta = await window.api.baixarAudio(url, 1)
+    //log.textContent += `\n[Sucesso] ${resposta}`
   } catch (err) {
-    log.textContent += `\n[Erro] ${err}`
+    //log.textContent += `\n[Erro] ${err}`
   }
 })
+
+
+function addBaixando() {
+
+}
+
+function addBaixados() {
+
+}
+
+function exBaixando() {
+  
+}
